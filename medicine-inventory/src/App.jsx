c2e319fac2ff3c9af65ce75e37982f26162252ab
LachlanEthanvas <166016001+LachlanEthanvas@users.noreply.github.com>
2025-05-31 11:43:48 +0530

    sorted in folder

--- medicine-inventory/src/App.jsx
+++ medicine-inventory/src/App.jsx
@@ -7,10 +7,10 @@ import Navbar from './components/Navbar'; // Import Navbar
 import SupplierRegisterForm from './components/supplier/Supplierregister';
 import LoginForm from './components/supplier/supplierLogin';
 import Dashboard from './components/supplier/SupplierDashboard';
-import InvoiceForm from './components/invoice'; // Import InvoicePage
-import InvoiceList from './components/invoicelist'; // Import InvoiceList
-import RegisterStaff from './components/staffregistration';
-import StaffLogin from './components/loginstaff'; // Import StaffLogin
+import InvoiceForm from './components/invoice/invoice'; // Import InvoiceForm
+import InvoiceList from './components/invoice/invoicelist'; // Import InvoiceList
+import RegisterStaff from './components/staff/staffregistration';
+import StaffLogin from './components/staff/loginstaff'; // Import StaffLogin
 
 
 function App() {
